830944eb3669748140a4742081a4212cd93a65b1
younguru <kiosun@mail.ru>
2022-03-20 23:54:53 +0300

    1

---
+++ src/main/java/ru/netology/Server.java
@@ -0,0 +1,93 @@
+package ru.netology;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private final List<String>
+            validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html",
+            "/forms.html", "/classic.html", "/events.html", "/events.js");
+
+    public void listen(int port, int nThreads) {
+        final var threadPool = Executors.newFixedThreadPool(nThreads);
+        try (final var serverSocket = new ServerSocket(port)) {
+            while (true) {
+                var socket = serverSocket.accept();
+                threadPool.submit(() -> processRequest(socket));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void processRequest(Socket socket) {
+        try {
+            final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            final var out = new BufferedOutputStream(socket.getOutputStream());
+            final var requestLine = in.readLine();
+            //  fix NullPointerException: Cannot invoke "String.split(String)" because "requestLine" is null
+            if (requestLine.isEmpty()) {
+                return;
+            }
+
+            final var parts = requestLine.split(" ");
+            if (parts.length != 3) {
+                return;
+            }
+            final var path = parts[1];
+
+            if (!validPaths.contains(path)) {
+                out.write((
+                        "HTTP/1.1 404 Not Found\r\n" +
+                                "Content-Length: 0\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.flush();
+                return;
+            }
+
+            final var filePath = Path.of(".", "public", path);
+            final var mimeType = Files.probeContentType(filePath);
+
+            if (path.equals("/classic.html")) {
+                final var template = Files.readString(filePath);
+                final var content = template.replace(
+                        "{time}",
+                        LocalDateTime.now().toString()
+                ).getBytes();
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + content.length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.write(content);
+                out.flush();
+                return;
+            }
+            final var length = Files.size(filePath);
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            Files.copy(filePath, out);
+            out.flush();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
